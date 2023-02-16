509ab4ae6adf01e8bbc19b8a895651fa25884e55
GeekyHacks <geekyhacks22@gmail.com>
2023-02-16 22:37:24 +0300

    push

--- countingValleyes.js
+++ countingValleyes.js
@@ -15,3 +15,27 @@ let valleyCounter = 0;
     }
     return valleyCounter;
 }
+/* 
+
+the credits of this code goes to @ktmmoorthi
+
+for this kind of problems you need to have a starting point  
+and a refrence point which are both represented by the altutude variable
+
+and since the output we want is how many valleys the hiker climbed we need 
+a counter as valleyCounter
+
+
+to go through all the path items you need for loop
+and since the path is a string here the ending point for the for loop will be 
+the length of it.
+
+since the altitude goes increase when hiker ckimbs up 
+altitude will be added in the first if statment and sustracted in the 
+the else brackets
+
+and valleycoubter must only start counting when the altitude at sealvl
+and equal zero
+
+the right place for return code is after the for loop so it gives the right 
+answer */
